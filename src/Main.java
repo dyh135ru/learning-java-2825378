7b9cb87dd48f8bd3cca2748ab426b45c7b650dc7
dyh135ru <dyruiz26@gmail.com>
2021-05-31 13:15:25 -0600

    Challenge completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
 
+    public static double calculateSalary(double hoursPerWeek,
+                                         double amountPerHour,
+                                         int vacationDays){
+
+        if(hoursPerWeek < 0){
+            return -1;
+        }
+
+        if(amountPerHour < 0){
+            return -1;
+        }
+        double weeklyPaycheck = hoursPerWeek * amountPerHour;
+        double unPaidTime = vacationDays * amountPerHour * 8;
+        return (weeklyPaycheck * 52) - unPaidTime;
+
+    }
+
     public static void main(String[] args) {
-        
+        double salary = calculateSalary(40,15, 8);
+        System.out.println(salary);
     }
 
 }
