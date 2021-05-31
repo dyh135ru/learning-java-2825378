2b6402ad8f816f6a0af2465dcf45441bebd1cc4d
dyh135ru <dyruiz26@gmail.com>
2021-05-31 11:22:07 -0600

    Challenge completed

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "In which state is located Mazatlan";
+        String choiceOne = "Durango";
+        String choiceTwo = "Sinaloa";
+        String choiceThree = "Jalisco";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
-
+        String input = scanner.next();
+        input = input.toLowerCase();
+        correctAnswer = correctAnswer.toLowerCase();
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
-
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+        if(correctAnswer.equals(input)){
+            // then the user is correct and we want to print out a congrats message to the user.
+            System.out.println("Congratulations, that is the correct answer!");
+        }else{
+            // If the user's input does not match the correctAnswer...
+            // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+            System.out.println("Sorry, the correct answer is: " + correctAnswer);
+        }
 
     }
 
